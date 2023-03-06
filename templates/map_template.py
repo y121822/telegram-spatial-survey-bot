fc3de5286d0a5476bf5551235bc001a789c2910b
y121822 <y121822@proton.me>
2023-03-06 09:46:29 +0600

    Imagery first v2

--- templates/map_template.py
+++ templates/map_template.py
@@ -60,11 +60,11 @@ html_3 = '''
             '<a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
-    }).addTo(map);
+    });
     var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/'+
     'MapServer/tile/{z}/{y}/{x}', {	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX,'+
     'GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
+    }).addTo(map);
     var Points = L.geoJSON(Source_point,{
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {icon: L.icon({
