7c66ad04b180c1f30730afcf405a242d742fa7d0
y121822 <y121822@proton.me>
2023-03-09 18:33:19 +0600

    Start.py edited

--- start.py
+++ start.py
@@ -7,12 +7,13 @@ load_dotenv()
 
 
 def main():
-    """Start the bot. In the case of a failure (e.g. caused by a database maintenance or
+    """Start the bot. In the case of failure (e.g. caused by a database maintenance or
        webhook issues), the bot at the current AWS EC2 instance will be restarted up
        to 20 times with increasing intervals between the tries. If the number of restarts
        reaches 20, the EC2 instance will be terminated. AWS EC2 Auto Scaling Group will
-       automatically launch another instance from the fully configured template and
-       the whole cycle will be repeated."""
+       automatically launch another instance from the fully configured template, the
+       present start.py file will be called by a cron job on startup and the whole cycle
+       will be repeated."""
     count = 21
     while True:
         if os.system(f"python {os.environ['BOT_PATH']}/bot.py"):
